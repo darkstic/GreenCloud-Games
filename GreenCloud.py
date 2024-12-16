bae42c378091ff3500a641560f078d6ad9d57b3f
darkstic <144748191+darkstic@users.noreply.github.com>
2024-12-16 10:55:44 -0700

    Update GreenCloud.py
    Added the ability to add custom browser games to list.

--- GreenCloud.py
+++ GreenCloud.py
@@ -1,10 +1,13 @@
 import webbrowser
 import time
+import os
+import os.path
+import ast
 
 online_features=False
 
 # First number is for main version features, Second is for overall list release wave, third is for total number of games.
-Version="1.1.10"
+Version="2.1.10 [Beta]"
 
 games_list=[
     ["Placeholder"],# 0
@@ -20,17 +23,68 @@ games_list=[
     ["Superhot Prototype","https://superhotgame.com/superhot-prototype"],#10
 ]
 
+
+dependency_path="C:\\Users\\Public\\Documents\\Greencloud_Custom.txt"
+
+with open(dependency_path, 'r') as file:
+    file_contents=file.read()
+
+file_contents=f"""[
+{file_contents}
+]
+"""
+
+custom_list= ast.literal_eval(file_contents.strip())
+
+for entry in custom_list:
+    games_list.append(entry)
+
+
+
+
 listsize=len(games_list)
+
+def custom_file():
+    addition=input("Enter custom game title here:  ")
+    print(" ")# Spacer
+    added_url=input("Enter the URL for custom game:  ")
+    print(" ")# Spacer
+
+    print(f"Is this correct? Name: {addition}, URL: {added_url}")
+    confirm_syntax=input("y/n:  ").lower()
+
+    if confirm_syntax!="y":
+        print("Custom game failed to add, relaunch to try again.")
+        time.sleep(3)
+        exit()
+    else:
+        dependency_path="C:\\Users\\Public\\Documents\\Greencloud_Custom.txt"
+        if os.path.isfile(dependency_path):
+            with open(dependency_path, 'a') as file:
+                file.write(f'["{addition}","{added_url}"],')
+        else:
+            with open(dependency_path, 'w') as file:
+                file.write(f'["{addition}","{added_url}"],')
+                
+    def internal_test(dependency_path):
+        with open(dependency_path, 'r') as file:
+            for line in file:
+                print(line.strip())
+
+    internal_test(dependency_path)
+
 
 def game_menu():
     for x, sublist in enumerate(games_list[1:listsize],start=1):
         print(f"{x}: {games_list[x][0]}")
 
+    print("Enter 'C' add custom games to list:")
     print("""
 
 
 
 """)# Large Spacer
+    
 
     game_selection=input("Enter the number for the game you would like to play:  ")
     if game_selection=="v":
@@ -42,6 +96,8 @@ def game_menu():
         game_menu()
     elif game_selection=="q":
         exit()
+    elif (game_selection=="C") or (game_selection=="c"):
+        custom_file()
     elif game_selection=="s":
         print("""
 
